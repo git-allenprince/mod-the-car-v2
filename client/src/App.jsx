71fd90a97d925ae881b752f0d8e7a7c617e8e81a
git-allenprince <prince.allen1820@gmail.com>
2025-10-16 14:05:22 +0530

    minor layout fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import CarList from "./components/CarList";
 import CarDetails from "./components/CarDetails";
 import Toolbar from "./components/Toolbar";
-import {useCarStore} from "./store/useCarStore";
+import { useCarStore } from "./store/useCarStore";
 import Logo from "./components/Logo";
 
 function App() {
@@ -9,20 +9,17 @@ function App() {
 
     return (
         <div className="min-h-screen bg-gray-100">
-            <div className=" p-2 flex items-center justify-start h-25">
+            <div className=" p-2 flex items-center justify-start">
                 <Logo />
-                <div className="p-3 ml-20 h-15 flex items-center w-3xl rounded-2xl shadow bg-white gap-5">
+                <div className="p-3 mx-4 md:mx-20 flex flex-wrap items-center rounded-2xl shadow bg-white gap-5">
                     <Toolbar />
                 </div>
             </div>
-            <div className="grid grid-cols-8 min-h-screen">
-                <div className="col-span-2"></div>
-                <div className="col-span-4 flex items-center justify-center">
-                    {/* <div className="translate-y-[-4rem]"> */}
-                        {selectedCarModel ? <CarDetails /> : "No car selected"}
-                    {/* </div> */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-8 min-h-screen">
+                <div className="col-span-1 md:col-span-6 flex items-center justify-center">
+                    {selectedCarModel ? <CarDetails /> : "No car selected"}
                 </div>
-                <div className="col-span-2"></div>
+                <div className="col-span-1 md:col-span-2"></div>
             </div>
 
             <footer className="mt-8"></footer>
--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,6 +1,6 @@
 export default function Logo() {
     return (
-        <header className="p-4">
+        <header className="mx-4">
             <h1 className="text-3xl font-bold font-poppins">Mod The Car.</h1>
             <p className="text-gray-600 pl-1">customize your rides.</p>
         </header>
